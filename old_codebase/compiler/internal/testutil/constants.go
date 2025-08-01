9f4283ebfd57b4112efce7ddb233741f27efdd52
Fuad Hasan <fuad.cs22@gmail.com>
2025-08-01 22:55:01 +0600

    remove old codebase

--- old_codebase/compiler/internal/testutil/constants.go
+++
@@ -1,9 +0,0 @@
-package testutil
-
-// Common error message formats for tests
-const (
-	ErrMsgFmt  = "%s = %v, want %v"
-	ErrNoNodes = "%s: expected nodes, got none"
-	ErrPanic   = "%s: expected no panic, got %v"
-	ErrNoPanic = "%s: expected panic, got none"
-)
